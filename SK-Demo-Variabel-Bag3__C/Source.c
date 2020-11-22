6cc4045dac170a364a730ca9f9c3ca9bec5be925
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2020-11-22 07:33:22 +0700

    Add project files.

---
+++ SK-Demo-Variabel-Bag3__C/Source.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int a;
+	char b;
+	float c;
+
+	puts("Memory locations:");
+	printf("A is at %p\n", &a);
+	printf("B is at %p\n", &b);
+	printf("C is at %p\n", &c);
+
+	_getch();
+	return(0);
+}
